b6ea6f369ee8fddbcca099c6910ef520bc5e38ed
Sergey Kostyuk <contact@s-kostyuk.me>
2017-07-27 15:35:44 +0300

    Hello World! Connected and received protocol version

---
+++ main_sync.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import socket
+
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 6600
+BUFFER_SIZE = 1024
+MESSAGE = "Hello, World!"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+
+# Returns something like this: b'OK MPD 0.19.0\n'
+data = s.recv(BUFFER_SIZE)  # type: bytes
+
+s.close()
+
+print("received data:", data)
